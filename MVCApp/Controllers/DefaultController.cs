d5458535031fad019de087323261684e09fb1fed
RMY <RMY@DESKTOP-O92QK0J>
2021-12-06 16:50:39 +0300

    Add project files.

---
+++ MVCApp/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+using System.Collections.Generic;
+
+namespace MVCApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var compList = new List<Computer>
+            {
+                new Computer() {
+                    CompID = 1,
+                    CompName ="Monster Abra A5",
+                    CompRAMName ="Kingston HyperX",
+                    CompRAMSize=8,
+                    CompGPUName="Nvidia GeForce GTX1050",
+                    CompGPUSize=6
+                },
+                new Computer() {
+                    CompID = 1,
+                    CompName ="Monster Abra A7",
+                    CompRAMName ="Kingston HyperX",
+                    CompRAMSize=16,
+                    CompGPUName="Nvidia GeForce GTX1070",
+                    CompGPUSize=6
+                },
+                new Computer() {
+                    CompID = 1,
+                    CompName ="Monster Tulpar T7",
+                    CompRAMName ="Kingston HyperX",
+                    CompRAMSize=32,
+                    CompGPUName="Nvidia GeForce GTX2070",
+                    CompGPUSize=8
+                }
+            };
+
+            return View(compList);
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCApp/Models/Computer.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCApp.Models
+{
+    public class Computer
+    {
+        public int CompID { get; set; }
+        public string CompName { get; set; }
+        public string CompRAMName { get; set; }
+        public int CompRAMSize { get; set; }
+        public string CompGPUName { get; set; }
+        public int CompGPUSize { get; set; }
+    }
+}
